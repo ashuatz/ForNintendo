f2a25d35664ef9f67ea4ddaa24bca36dc0b40888
정지상 <ashuatz@gmail.com>
2020-12-17 22:12:40 +0900

    * 프리팹 수정, 사운드파일, 텍스트

---
+++ Assets/Scripts/AlwaysIdentity.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysIdentity : MonoBehaviour
+{
+    void Update() => transform.rotation = Quaternion.identity;
+}
