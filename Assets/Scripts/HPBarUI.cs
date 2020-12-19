68e3842c9ff1ebb53add93d9a3959b701d448e8a
정지상 <ashuatz@gmail.com>
2020-12-19 23:38:37 +0900

    *color data

--- Assets/Scripts/HPBarUI.cs
+++ Assets/Scripts/HPBarUI.cs
@@ -6,11 +6,20 @@ using UnityEngine.UI;
 
 public class HPBarUI : MonoBehaviour
 {
+
+    [Header("init")]
+    [SerializeField]
+    private Image BackgroundImage;
+
+
     [Serializable]
     public class HPGauge
     {
-        public List<GameObject> Cells;
+        public List<Image> Cells;
     }
+
+    [SerializeField]
+    private HPColorData colorData;
 
     [SerializeField]
     private float HPPerCell;
@@ -23,11 +32,49 @@ public class HPBarUI : MonoBehaviour
 
     private void Awake()
     {
+        ColorInit();
+
         entity.HP.OnDataChanged += HP_OnDataChanged;
 
         HP_OnDataChanged(entity.HP.CurrentData);
 
         LayoutRebuilder.ForceRebuildLayoutImmediate(transform as RectTransform);
+    }
+
+    private void ColorInit()
+    {
+        HPColorData.HPColor colorInfo = new HPColorData.HPColor();
+
+        switch (entity)
+        {
+            case TestPlayer p:
+                colorInfo = colorData.colors.Find((data) => data.myType == HPColorData.HPType.Player);
+                break;
+            
+            case TestMinion m:
+                colorInfo = colorData.colors.Find((data) => data.myType == HPColorData.HPType.Minion);
+                break;
+            
+            case TestEnemy e:
+            case SpecialEnemyTypeA s:
+                colorInfo = colorData.colors.Find((data) => data.myType == HPColorData.HPType.Enemy);
+                break;
+            
+            case TestNPC n:
+                colorInfo = colorData.colors.Find((data) => data.myType == HPColorData.HPType.NPC);
+                break;
+
+            default: return;
+        }
+
+        BackgroundImage.color = colorInfo.BackgroundColor;
+        for (int i = 0; i < 3; i++)
+        {
+            foreach (var cell in Gauge[i].Cells)
+            {
+                cell.color = colorInfo.GetColor(i);
+            }
+        }
     }
 
     private void HP_OnDataChanged(float currentHP)
@@ -47,7 +94,7 @@ public class HPBarUI : MonoBehaviour
         {
             foreach (var cell in phase.Cells)
             {
-                cell.SetActive(count > 0);
+                cell.gameObject.SetActive(count > 0);
                 count -= 1;
             }
         }
