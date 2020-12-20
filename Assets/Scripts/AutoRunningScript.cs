472f819eac378ee755125c62999e9af431ada872
정지상 <ashuatz@gmail.com>
2020-12-20 22:41:59 +0900

    *zombie color, muzi color, splashImage setting

---
+++ Assets/Scripts/AutoRunningScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRunningScript : MonoBehaviour
+{
+    [SerializeField]
+    private WayPointData data;
+
+    private IEnumerator Start()
+    {
+        float t = 0;
+
+        foreach (var message in data.Messages)
+        {
+            if (t < message.time)
+                yield return new WaitForSeconds(message.time - t);
+            t += message.time;
+
+            ShowMessage(message.desc, message.duration);
+
+            yield return new WaitForSeconds(message.duration);
+            t += message.duration;
+        }
+    }
+
+    private void ShowMessage(string message, in float time)
+    {
+        TextBox.Instance.Input(message, time);
+    }
+}
