6e1b266c25b089c87b0e07d2f1760ae20a193e77
ePEa1 <zmzm5442@gmail.com>
2020-12-20 22:37:34 +0900

    엔딩 완성 - Prefabs/Ending 프리팹 추가하셈

--- Assets/Scripts/EndingManager.cs
+++ Assets/Scripts/EndingManager.cs
@@ -13,25 +13,31 @@ public class EndingManager : MonoBehaviour
     [SerializeField] Image[] _clearImg;
     [SerializeField] float _imgFadeSpeed = 1.0f;
 
+    [SerializeField] Text _timeName;
     [SerializeField] Text _timeText;
 
+    [SerializeField] Text _rankName;
     [SerializeField] Image _rankImg;
-    [SerializeField] Image[] _rankImgs;
+    [SerializeField] Sprite[] _rankImgs;
 
-    [SerializeField] Rank[] _ranks;
+    [SerializeField] int[] _rankTime;
 
-    void Start()
+    public bool _IsEnding { get; private set; }
+
+    void Awake()
     {
-        
+        _IsEnding = false;
     }
 
     IEnumerator Ending()
     {
+        _IsEnding = true;
+
         float t = 0;
 
         while (t < 1)
         {
-            t += Mathf.Min(1, t + Time.deltaTime * _fadeSpeed);
+            t = Mathf.Min(1, t + Time.deltaTime * _fadeSpeed);
             _background.color = new Color(0, 0, 0, t * 0.7f);
 
             yield return true;
@@ -41,20 +47,44 @@ public class EndingManager : MonoBehaviour
 
         while(t<1)
         {
-            t += Mathf.Min(1, t + Time.deltaTime * _imgFadeSpeed);
+            t = Mathf.Min(1, t + Time.deltaTime * _imgFadeSpeed);
             for (int i = 0; i < _clearImg.Length; i++)
                 _clearImg[i].color = new Color(1, 1, 1, t);
 
             yield return true;
         }
 
+        _timeName.gameObject.SetActive(true);
+        _timeText.gameObject.SetActive(true);
+        int sec = 0;
+        while (sec < _timer.CurrentTime)
+        {
+            sec++;
+            _timeText.text = Mathf.FloorToInt(sec / 60).ToString() + ":" + sec % 60;
 
+            yield return true;
+        }
+
+        t = 0;
+        while(t<1)
+        {
+            t += Time.deltaTime;
+
+            yield return true;
+        }
+
+        _rankName.gameObject.SetActive(true);
+        _rankImg.gameObject.SetActive(true);
+        for (int i = 0; i < _rankTime.Length; i++)
+        {
+            if (sec <= _rankTime[i])
+            {
+                _rankImg.sprite = _rankImgs[i];
+                break;
+            }
+            else if (i >= _rankTime.Length - 1)
+                _rankImg.sprite = _rankImgs[i];
+
+        }
     }
-}
-
-[System.Serializable]
-public class Rank
-{
-    public string _rank;
-    public string _time;
 }
